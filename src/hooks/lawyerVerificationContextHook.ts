5cbfc4aff278f61c0b811dcd40f2b1e8ae3a7203
Ansar1503 <muhammedansarma1503@gmail.com>
2025-08-26 18:36:02 +0530

    verification context hook removed

--- src/hooks/lawyerVerificationContextHook.ts
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-import { LawyerVerificationContext } from "@/context/LawyerVerificationContext";
-
-export const useLawyerVerification = () => {
-  const context = useContext(LawyerVerificationContext);
-  return context;
-};
