61239184de11507b20638f4a6bc9f9fd7d6d1836
Ansar1503 <muhammedansarma1503@gmail.com>
2025-08-27 12:33:54 +0530

    fetch notification when navbar renders

--- src/components/NotificationPopover.tsx
+++ src/components/NotificationPopover.tsx
@@ -27,7 +27,7 @@ export default function NotificationComponent() {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
   const { mutateAsync: JoinSessionMutation } = useJoinSession();
-  const { data: notificationsData } = useInfiniteFetchAllNotifications(isOpen);
+  const { data: notificationsData } = useInfiniteFetchAllNotifications(true);
   const { mutateAsync: MarkAsRead } = useUpdateReadNotification();
   const { mutateAsync: MarkAllAsRead } = useUpdateMarkAllAsRead();
   const notifications = notificationsData?.pages?.flatMap(
