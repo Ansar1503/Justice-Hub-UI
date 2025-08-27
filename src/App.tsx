1fdca86a556686601a32fef7d1ee3bbef025f193
Ansar1503 <muhammedansarma1503@gmail.com>
2025-08-27 16:50:38 +0530

    app updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import UserRoutes from "./Routes/user_routes/User.Routes";
-import { useContext } from "react";
-// import { ThemeContext } from "./context/ThemeProvider";
-import { SocketContext } from "./context/SocketProvider";
 import { Toaster } from "react-hot-toast";
 
 function App() {
-  // const { theme } = useContext(ThemeContext);
-  useContext(SocketContext);
   return (
     <>
-      <Toaster  position="bottom-right"  />
+      <Toaster position="bottom-right" />
       <Routes>
         <Route path="/*" element={<UserRoutes />} />
       </Routes>
